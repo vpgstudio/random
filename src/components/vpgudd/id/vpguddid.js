f69c4a7edce35a803e3c8cd134c65fe4a5f7b23d
vpg-code <markus.vind600@gmail.com>
2020-11-13 17:21:15 +0000

    Added 404 page for multipel page suporte

--- src/components/vpgudd/id/vpguddid.js
+++ src/components/vpgudd/id/vpguddid.js
@@ -4,7 +4,7 @@ export class vpguddid extends Component {
   render() {
     return (
       <div>
-        <h3>Id for loding data = {this.props.match.params.id}</h3>
+        <h3>Id for loding data = {this.props.match.params.added_params}</h3>
       </div>
     );
   }
