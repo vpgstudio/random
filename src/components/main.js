f8b22145cc32420aa534540abd2c3e11b6d61fcb
vpg-code <markus.vind600@gmail.com>
2020-11-13 17:03:19 +0000

    Edited all files

--- src/components/main.js
+++ src/components/main.js
@@ -1,2 +1,3 @@
-export { default as Main} from "./main/main"
-export { default as Vpgudd} from "./vpgudd/main"
+export { default as Main } from "./main/main";
+export { default as Vpgudd } from "./vpgudd/main";
+export { default as VpguddId } from "./vpgudd/id/vpguddid";
